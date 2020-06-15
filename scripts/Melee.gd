4f4cd9f58f542f0bc39ef4c9e156432583d735d4
duwaar <duwaar@yahoo.com>
2020-06-15 16:55:14 -0700

    Moved hit and damage code into separate functions, and added Ulrich's Fury.

--- scripts/Melee.gd
+++ scripts/Melee.gd
@@ -1,6 +1,9 @@
 extends VBoxContainer
 
 
+var message = ""
+var damage = 0
+
 
 func get_hit_location(hit_roll):
 	return $"/root/global".get_hit_location(hit_roll)
@@ -14,19 +17,49 @@ func set_hit_output(message):
 	$OutputLabel.text = message
 
 
-func _on_Attack_button_up():
-	var message = ""
-	var damage = $BaseDamage.get_value() + d10() + 1
-	
-	var hit_roll = d10()*10 + d10()
+func roll_to_hit(var ulrich = false):
+	var results = ""
+	var hit = false
+	var hit_roll = d10()*10 + d10() + 1
 	var hit_chance = $WeaponSkill.get_value() + $HitModifier.get_value()
 	var margin = hit_chance - hit_roll
 	
-	message += "Roll: " + str(hit_roll) + " against " + str(hit_chance)
+	results += "Roll: " + str(hit_roll) + " against " + str(hit_chance)
 	if margin >= 0:
-		message += "\n" + "Hit the " + get_hit_location(hit_roll) + " by " + str(margin) + "%"
-		message += "\n" + "Damage: " + str(damage)
+		results += "\n" + "Hit the " + get_hit_location(hit_roll - 1) + " by " + str(margin) + "%"
+		hit = true
 	else:
-		message += "\n" + "Missed by " + str(-1 * margin) + "%"
+		results += "\n" + "Missed by " + str(-1 * margin) + "%"
+		hit = false
+	
+	if not ulrich:
+		message += results
+	return hit
+
+
+func roll_damage():
+	damage = 0
+	var roll = d10() + 1
+	damage += $BaseDamage.get_value() + roll
+	
+	if roll == 10 and roll_to_hit(true):
+		ulrichs_fury()
+	
+	message += "\n" + "Damage: " + str(damage)
+	
+
+func ulrichs_fury():
+	message += "\n" + "Ulrich's Fury!"
+	var roll = d10() + 1
+	damage += roll
+	if roll == 10:
+		ulrichs_fury()
+		
+	
+func _on_Attack_button_up():
+	message = ""
+	var hit = roll_to_hit()
+	if hit:
+		roll_damage()
 	set_hit_output(message)
 	
