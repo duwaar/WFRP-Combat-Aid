979d7b2b6572a9c3c429ac3e12224f3f974ec4a1
duwaar <duwaar@yahoo.com>
2020-06-15 19:26:07 -0700

    Cleared out orphan scripts and renamed one after making some major changes.

--- scripts/DamageBox.gd
+++
@@ -1,9 +0,0 @@
-extends SpinBox
-
-
-func _on_PlusButton_button_up():
-	self.value += 1
-
-
-func _on_MinusButton_button_up():
-	self.value -= 1
--- scripts/Finder.gd
+++ scripts/Finder.gd
@@ -1,12 +1,73 @@
-extends Node2D
+extends VBoxContainer
+
+var crit_effects
+var crit_values
+var timer = 0.0
+var printed = false
+var message = ""
 
 
 
-func _on_BackButton_button_up():
-	$HelpPage.visible = false
-	$ControlPanel.visible = true
+func load_from_json(path):
+	return $"/root/global".load_from_json(path)
+
+func _ready():
+	crit_effects = load_from_json("res://resources/critical_effects.json")
+	crit_values = load_from_json("res://resources/critical_values.json")
+	
+	
+func _process(delta):
+	if not printed:
+		timer -= delta
+		if timer < 0:
+			$Output.text = message
+			message = ""
+			printed = true
 
 
-func _on_HelpButton_button_up():
-	$ControlPanel.visible = false
-	$HelpPage.visible = true
+func d10():
+	return $"/root/global".d10()
+
+
+func get_hit_location(hit_roll):
+	return $"/root/global".get_hit_location(hit_roll)
+
+
+func get_critical_value(crit_damage, percent):
+	return int(crit_values[floor(percent / 10)][int(crit_damage)])
+
+
+func get_critical_effect(hit_location, crit_value):
+	if "arm" in hit_location:
+		hit_location = "arm"
+	elif "leg" in hit_location:
+		hit_location = "leg"
+	
+	if $HBoxContainer/Damage.get_value() == 0:
+		return "No critical."
+	else:
+		return str(crit_effects[hit_location][crit_value - 1])
+
+
+func _on_CalculateButton_button_up():
+	if timer < 0:
+		var location = get_hit_location(int($HBoxContainer/Hit.get_value()))
+		message += "Location: " + location + "\n"
+	
+		var crit_value
+		if int($HBoxContainer/Hit.get_value()) == 0:
+			crit_value = 0
+		else:
+			var effect_roll = d10() * 10 + d10() + 1
+			crit_value = int( crit_values[floor(effect_roll / 10)][int($HBoxContainer/Damage.get_value()) - 1] )
+			message += "Critical Effect Roll: " + str(effect_roll) + "\n"
+			message += "Critical Value: " + str(crit_value) + "\n"
+	
+		var crit_effect = get_critical_effect(location, crit_value)
+		message += "\n"
+		message += crit_effect
+	
+		$Output.text = "Loading..."
+		timer = 0.5
+		printed = false
+	
